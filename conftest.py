fc8761fc72ba9d2dcb8632b2f2325f7771e44e4f
Alex Pokrashenko <satrey.mail@gmail.com>
2025-03-12 08:14:48 +0500

    Добавлены опции  Chrome драйвера в conftest.py

--- conftest.py
+++ conftest.py
@@ -4,6 +4,10 @@ from selenium import webdriver
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-notifications")
+
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
